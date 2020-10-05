617587cf48c2ec9c414f58f42694ab7a75639a25
Nam Vu <vungocnam0409@gmail.com>
2020-10-05 21:53:19 +0700

    add attribute for model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+    has_many :ingredients
+    accepts_nested_attributes_for :ingredients, 
+        allow_destroy: true, dependent: :destroy
+        
+    has_many :steps
+        accepts_nested_attributes_for :steps, 
+            allow_destroy: true, dependent: :destroy
 end
