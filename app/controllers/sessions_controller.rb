b4c2fd0b353ffb3f22c1ce02c689428510165cb4
Nam Vu <vungocnam0409@gmail.com>
2020-10-23 12:29:25 +0700

    using chef.authenticate for check valid password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    chef = Chef.find_by(mail: params[:session][:mail].downcase) 
+    if chef && chef.authenticate(params[:session][:password])
+        redirect_to recipes_path
+    else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render action: 'new' 
+    end
+  end
+
+  def destroy
+  end
+
 end
