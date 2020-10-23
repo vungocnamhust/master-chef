4b2d9d8a94958ebdc246566e15f8d121e1d42e41
Nam Vu <vungocnam0409@gmail.com>
2020-10-23 10:02:12 +0700

    Update model, controller, UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   
   resources :recipes
   resources :ingredient
-  resources :chefs
-  root 'chefs#new'
+  root 'chefs#index'
 
   post '/add-recipe', to: "recipes#createRecipe"
   delete '/ingredient/:id/delete', to: "ingredient#destroy"
   delete '/steps/:id/delete', to: "recipes#destroyStep"
   post '/search', to: "recipes#index"
-  get '/signup', to: "chefs#new"
+
+  get '/signup', to: "chefs#new",as: :newChef
+  post '/signup', to: "chefs#create",as: :createChef
+  get '/chefs/:id', to: "chefs#show",as: :showChef
+  get '/chefs/index', to: "chefs#index",as: :indexChef
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_082544) do
+ActiveRecord::Schema.define(version: 2020_10_22_101247) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_10_22_082544) do
     t.integer "phone"
     t.text "mail"
     t.text "password"
+    t.text "password_digest"
   end
 
   create_table "ingredient_recipes", force: :cascade do |t|
