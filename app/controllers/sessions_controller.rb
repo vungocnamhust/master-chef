9d4776723ec416e180d44773d624b5d0d94ab7f5
Nam Vu <vungocnam0409@gmail.com>
2020-10-27 17:44:21 +0700

    Clean code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,10 @@ class SessionsController < ApplicationController
   include SessionsHelper
 
   def new
-    p '___________________'
-    p '___________________'
-    p 'session new'
-    p '___________________'
-
   end
 
   def create
     chef = Chef.find_by(mail: params[:session][:mail].downcase)
-    p '___________________'
-    p '___________________'
-    p chef
-    p '___________________'
-
     if chef&.authenticate(params[:session][:password])
       log_in(chef)
       redirect_to recipes_path
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,18 +4,10 @@ module SessionsHelper
   end
 
   def current_chef
-    p 'current chef ____________________'
-    p @current_chef
-    p '____________________'
-    p '____________________'
     @current_chef ||= Chef.find_by(id: session[:chef_id]) if session[:chef_id]
   end
 
   def logged_in?
-    p '++++++++++++++++++++'
-    p current_chef.nil?
-    p '____________________'
-
     !current_chef.nil?
   end
 
