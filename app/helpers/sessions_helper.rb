40449ca80c1efd59455d07d98210063fb5cf67ff
Nam Vu <vungocnam0409@gmail.com>
2020-10-27 17:40:40 +0700

    Fix bug: save session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,22 @@
 module SessionsHelper
   def log_in(chef)
-    session[:chef_id] = chef.id
+    session[:chef_id] = chef.id    
   end
 
   def current_chef
-    @current_chef ||= Chef.find_by(id: session[:chef_id]) if session[:chef_id]
-    p '____________________'
+    p 'current chef ____________________'
     p @current_chef
     p '____________________'
     p '____________________'
-
+    @current_chef ||= Chef.find_by(id: session[:chef_id]) if session[:chef_id]
   end
 
   def logged_in?
-    !@current_chef.nil?
+    p '++++++++++++++++++++'
+    p current_chef.nil?
+    p '____________________'
+
+    !current_chef.nil?
   end
 
   def log_out
