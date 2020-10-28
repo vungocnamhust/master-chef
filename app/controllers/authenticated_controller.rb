778759ae374cdeec0571c77d2d09fa19e195fa03
Nam Vu <vungocnam0409@gmail.com>
2020-10-28 14:27:24 +0700

    Remove authenticate controller

--- app/controllers/authenticated_controller.rb
+++
@@ -1,9 +0,0 @@
-class AuthenticatedController < ApplicationController
-  before_action :currentUser
-
-  def currentUser
-    # check session
-    # parse session => user
-    # @currentUser = user
-  end
-end
