30a62d516ab847e9bdfd15cda2dad17d7fc2ba4f
Nam Vu <vungocnam0409@gmail.com>
2020-10-28 15:35:11 +0700

    Add method for creating token

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,12 +1,12 @@
 class Chef < ApplicationRecord
   before_save { self.mail = mail.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :mail, presence: true, length: { maximum: 255 },
-                   format: { with: VALID_EMAIL_REGEX },
-                   uniqueness: true
+  validates :mail, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: {minimum: 6}
   # Do not need to understand cost or st else
   # This method used for validating password by some restrictions
   def self.digest(string)
@@ -15,5 +15,10 @@ class Chef < ApplicationRecord
            else
              BCrypt::Password.create(string, cost: cost)
            end
+  end
+
+  # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
 end
